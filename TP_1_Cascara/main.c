db8fb1746013768ab1f590e435e2d2dd508eb89f
NicolasGimenez <36281564+NicolasGimenez@users.noreply.github.com>
2018-03-28 22:06:29 -0300

    todo el tp 1

---
+++ TP_1_Cascara/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+
+    while(seguir=='s')
+    {
+        printf("1- Ingresar 1er operando (A=x)\n");
+        printf("2- Ingresar 2do operando (B=y)\n");
+        printf("3- Calcular la suma (A+B)\n");
+        printf("4- Calcular la resta (A-B)\n");
+        printf("5- Calcular la division (A/B)\n");
+        printf("6- Calcular la multiplicacion (A*B)\n");
+        printf("7- Calcular el factorial (A!)\n");
+        printf("8- Calcular todas las operacione\n");
+        printf("9- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                /**Ingresa el numero*/
+                /**BanderaNUmeroUno = 1*/
+
+                break;
+            case 2:
+                /**Ingresa el numero 2*/
+                /**BanderaNUmeroDos = 1*/
+                break;
+            case 3:
+                /**Si FlagNumeroUno == 1 && Flasgasjhoiahsdo == 1  Sumar Si no */
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                seguir = 'n';
+                break;
+        }
+    }
+
+
+    return 0;
+}
